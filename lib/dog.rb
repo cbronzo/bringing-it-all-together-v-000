516115db6bafcdaadd61d83a662e0c6dd881b180
Catherine Bronzo <cbronzo@mac.com>
2018-07-27 04:24:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,12 +55,13 @@ class Dog
     SELECT *
     FROM dogs
     WHERE id = ?
-    SQL
+      SQL
       row = DB[:conn].execute(sql, id)[0]
     self.new_from_db(row)
  end
  
  def self.find_or_create_by
+   
  end
   
   def self.find_by_name(name)
