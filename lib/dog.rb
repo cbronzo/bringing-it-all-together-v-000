fa18ab73836c58c8b32aceef5e1845903f3ca0d5
Catherine Bronzo <cbronzo@mac.com>
2018-07-27 04:20:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,9 +66,9 @@ class Dog
   
   
   def self.new_from_db(row)
-    dog.id = row[0]
-    dog.name = row[1]
-    dog.breed = row[2]
+    id = row[0]
+    name = row[1]
+    breed = row[2]
     self.new(id: id, name: name, breed: breed)
   end
   
