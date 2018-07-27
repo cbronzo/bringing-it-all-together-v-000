ef4ec4fe8eba3ba3ed4a65a05611f42ebff40450
Catherine Bronzo <cbronzo@mac.com>
2018-07-27 04:23:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,7 +50,15 @@ class Dog
  end
  
  
-
+ def self.find_by_id
+   sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    SQL
+    row = DB[:conn].execute(sql, id)[0]
+    self.new_from_db(row)
+ end
  
  def self.find_or_create_by
  end
